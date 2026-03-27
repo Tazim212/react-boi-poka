235d0afde77f240c7ffe1d203949f3dba20b4660
Tazim212 <iftekhartazim17@gmail.com>
2026-03-27 17:40:50 +0600

    navbar done

---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            hm
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { LuAlignLeft } from 'react-icons/lu';
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+    return (
+        <div className='bg-linear-to-l from-blue-950 to-cyan-500'>
+            <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+
+            <div className="flex justify-around items-center">
+                <label htmlFor="my-drawer-1" className="btn btn-accent lg:hidden"><LuAlignLeft className='text-2xl'></LuAlignLeft></label>
+                
+                <h1 className='text-2xl font-bold'>Book Vibes</h1>
+                <ul className="hidden lg:flex px-10">
+                    <li className='px-2'>Home</li>
+                    <li className='px-2'>Listed Books</li>
+                    <li>Pages To Read</li>
+                </ul>
+
+                <div className='my-3'>
+                    <button className='btn btn-secondary mr-6 rounded-md'>Sign In</button>
+                    <button className='btn btn-success rounded-md'>Sign Up</button>
+                </div>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-linear-to-t from-emerald-700 to-blue-800 min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
+                    <li className='pb-1'>Home</li>
+                    <li className='pb-2'>Listed Books</li>
+                    <li>Pages To Read</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
+
+{/* <NavLink><li className='px-2'>Home</li></NavLink>
+                    <NavLink><li className='px-2'>Listed Books</li></NavLink>
+                    <NavLink><li>Pages To Read</li></NavLink> */}
